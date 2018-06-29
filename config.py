5e3ebacf4631d5f08ecd8fa01791e18206680f5f
ssj5638 <ssj5638@gmail.com>
2018-06-30 03:43:20 +0900

    .

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ CONFIG = {
     'result_directory_v': '__results__/visualization',
     'common':{
         'base_url':'https://graph.facebook.com/v3.0',
-        'access_token': "EAACEdEose0cBAH2TpS3buZAlHBfW47PCIKYmZAqL1SLWmGsfC29dAT61I50jPPV2G4mqwzBDTHM84ZBo3yeeAQIAEuf8OojhZAkrzINzoo4g8JHaPyMMRhBR7zaZBC5Eg5R18bsqd7QwzKRBzDuMXBWNuRjxZCdxAvsraLlT2lAZCQEdC4YqpJV5ExTYipvzf4ZD",
-        'fetch':True,
+        'access_token': "EAACEdEose0cBAIQaAPd002gPHZAK7ZCxQcIipfSeUjxN4IA5WpZAz08JWwfB9klILRT14kfydHvjCPsh2b0Dk13qDCv1CmsRnl6Y7b0jZAzGs3QZA3lAUYZBYo56s3vBR6UfQFpHreH18oGFxZAA9AJsi5ZBrEZBhp10WXGnM8ec0ZCJySjT1MKaztJZBSJiWonZCzAZD",
+        'fetch':False,
         'result_directory_c': '__results__/crawling'
     }
 }
